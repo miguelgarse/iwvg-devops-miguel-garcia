8c303bf5721e79e8bc864595fd357ccb3687d071
Miguel Garcia <miguelgarse@gmail.com>
2020-09-30 19:29:15 +0200

    Implementation of method findUserFamilyNameByImproperFraction() with its tests #10

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -2,6 +2,8 @@ package es.upm.miw.iwvg_devops.code;
 
 import org.apache.logging.log4j.LogManager;
 
+import java.util.stream.Stream;
+
 public class Searches {
 
     public Fraction findFractionMultiplicationByUserFamilyName(String familyName) {
@@ -11,4 +13,13 @@ public class Searches {
                 .peek(x -> LogManager.getLogger(this.getClass()).info("Fraction: " + x.getNumerator() + " / " + x.getDenominator()))
                 .reduce(Fraction::multiply).orElse(new Fraction(0,0));
     }
+
+    public Stream<String> findUserFamilyNameByImproperFraction() {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream().anyMatch(Fraction::isImproper))
+                .map(User::getFamilyName)
+                .distinct()
+                .peek(x -> LogManager.getLogger(this.getClass()).info("Family name: " + x));
+    }
+
 }
--- src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -1,7 +1,15 @@
 package es.upm.miw.iwvg_devops.code;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SearchesTest {
 
@@ -12,5 +20,15 @@ public class SearchesTest {
 
         assertEquals(0, searchResult.getNumerator());
         assertEquals(0, searchResult.getDenominator());
+    }
+
+    @Test
+    void testFindUserFamilyNameByImproperFraction(){
+        Searches searches = new Searches();
+        List<String> familyNameList = Arrays.asList("Fernandez", "Blanco", "López", "Torres");
+
+        Stream<String> searchResult = searches.findUserFamilyNameByImproperFraction();
+
+        assertTrue(familyNameList.containsAll(searchResult.collect(Collectors.toList())));
     }
 }
